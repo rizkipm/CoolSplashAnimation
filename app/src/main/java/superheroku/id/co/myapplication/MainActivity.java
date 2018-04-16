5185057c89dc61c962a9dce5dc7cf88c32f93654
Programmer Muslim <rizki@imastudio.co.id>
2018-04-16 20:09:35 +0700

    add

--- app/src/main/java/superheroku/id/co/myapplication/MainActivity.java
+++ app/src/main/java/superheroku/id/co/myapplication/MainActivity.java
@@ -1,13 +1,97 @@
 package superheroku.id.co.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //pembuatan variabel untuk linearlayout
+    private LinearLayout lv_loading;
+    private AVLoadingIndicatorView avi;
+    private static final String[] INDICATORS=new String[]{
+            "BallPulseIndicator",
+            "BallGridPulseIndicator",
+            "BallClipRotateIndicator",
+            "BallClipRotatePulseIndicator",
+            "SquareSpinIndicator",
+            "BallClipRotateMultipleIndicator",
+            "BallPulseRiseIndicator",
+            "BallRotateIndicator",
+            "CubeTransitionIndicator",
+            "BallZigZagIndicator",
+            "BallZigZagDeflectIndicator",
+            "BallTrianglePathIndicator",
+            "BallScaleIndicator",
+            "LineScaleIndicator",
+            "LineScalePartyIndicator",
+            "BallScaleMultipleIndicator",
+            "BallPulseSyncIndicator",
+            "BallBeatIndicator",
+            "LineScalePulseOutIndicator",
+            "LineScalePulseOutRapidIndicator",
+            "BallScaleRippleIndicator",
+            "BallScaleRippleMultipleIndicator",
+            "BallSpinFadeLoaderIndicator",
+            "LineSpinFadeLoaderIndicator",
+            "TriangleSkewSpinIndicator",
+            "PacmanIndicator",
+            "BallGridBeatIndicator",
+            "SemiCircleSpinIndicator",
+            "com.wang.avi.sample.MyCustomIndicator"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        lv_loading = (LinearLayout) findViewById(R.id.lv_loading);
+
+//        String indicator=getIntent().getStringExtra("indicator");
+        avi= (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.setIndicator("BallClipRotateMultipleIndicator");
+//        avi.setIndicator("PacmanIndicator");
+
+
+//        //pembuatan animasi Bounce (atas bawahh)
+//        new BounceAnimation(lv_loading)
+//                .setBounceDistance(50)
+//                .setDuration(3000)
+//                .animate();
+
+        //membuat sebuah proses yang ter delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //mendefenisikan Intent activity
+                Intent i = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(i);
+
+                //finish berguna untuk mengakhiri activity
+                //disini saya menggunakan finish,supaya ketika menekan tombol back
+                //tidak kembali pada activity SplashScreen nya
+                finish();
+            }
+            //disini maksud 3000 nya itu adalah lama screen ini terdelay 3 detik,dalam satuan mili second
+        }, 3000);
+    }
+
+    public void hideClick(View view) {
+        avi.hide();
+        // or avi.smoothToHide();
+    }
+
+    public void showClick(View view) {
+        avi.show();
+        // or avi.smoothToShow();
     }
 }
